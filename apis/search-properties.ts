19981f34325413ba1734a61274987b81e0517937
Rahat Saqib <rahatsaqib78@gmail.com>
2023-06-21 16:32:56 +0600

    removed consoles

--- apis/search-properties.ts
+++ apis/search-properties.ts
@@ -24,7 +24,6 @@ const searchPropertiesApi = async (req: Request, res: Response) => {
         })
     }
     catch (err) {
-        console.log(err)
         res.status(500).json({
             success: false,
             data: []
